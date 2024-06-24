1202da7bbf5117948a4b0a32d282248b803a0b11
Tran Dang Duat <23020023@vnu.edu.vn>
2024-06-24 20:03:35 +0700

    Add necessary middlewares

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 
 const app = express();
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.session());
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
     res.render("index");
